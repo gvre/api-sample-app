ad18eb4f26dba2e00412b5ef3dec4c7a7b1f2d6c
Giannis Vrentzos <gvre@gvre.gr>
2020-08-26 14:15:31 +0300

    Add health check endpoints

--- cmd/rest/api/routes.go
+++ cmd/rest/api/routes.go
@@ -3,6 +3,16 @@ package api
 // CreateRoutes creates all app routes.
 func (s *Server) CreateRoutes() {
 	s.Router.Handle(
+		"/-/live",
+		s.EnableCORS(s.HandleCheckLive()),
+	).Methods("GET")
+
+	s.Router.Handle(
+		"/-/health",
+		s.EnableCORS(s.HandleCheckHealth()),
+	).Methods("GET")
+
+	s.Router.Handle(
 		"/users",
 		s.EnableCORS(s.Compress(s.HandleGetUsers())),
 	).Methods("GET")
